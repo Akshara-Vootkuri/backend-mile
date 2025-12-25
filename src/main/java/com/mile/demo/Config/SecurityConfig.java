a956a36b2d86cad736a1903349a5ce03ea97bb5d
Akshara Reddy <aksharavooturi25@gmail.com>
2025-12-25 09:49:42 +0530

    allow cors

--- src/main/java/com/mile/demo/Config/SecurityConfig.java
+++ src/main/java/com/mile/demo/Config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://mile-front.onrender.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://intership-frontend-9k6x.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- src/main/java/com/mile/demo/controller/AuthController.java
+++ src/main/java/com/mile/demo/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins ={ "http://localhost:5173","https://mile-front.onrender.com"})
+@CrossOrigin(origins ={ "http://localhost:5173","https://intership-frontend-9k6x.onrender.com"})
 //@CrossOrigin(origins = "*")
 public class AuthController {
 
