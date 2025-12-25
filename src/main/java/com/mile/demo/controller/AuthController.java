8f8a0a7781e8a9d8b71f07951f865f0125fece68
Akshara Reddy <aksharavooturi25@gmail.com>
2025-12-25 09:29:03 +0530

    changed cors

--- src/main/java/com/mile/demo/controller/AuthController.java
+++ src/main/java/com/mile/demo/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins ={ "http://localhost:5173","https://mile-front.onrender.com"})
 //@CrossOrigin(origins = "*")
 public class AuthController {
 
